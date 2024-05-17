35974f27b2604915bc1027e2df123aa9f78afe96
usman <usman@gmail.com>
2024-05-17 17:57:27 +0500

    code refector in some sections

--- frontend/src/components/whatWeProvide/Card.jsx
+++ frontend/src/components/whatWeProvide/Card.jsx
@@ -1,26 +1,59 @@
+// import { FaPlus } from "react-icons/fa";
+
+// const Card = ({ title, description, image }) => {
+//   return (
+//     <div className="cursor-pointer relative w-[160px] h-[207.3px] lg:w-[281.44px] lg:h-[363.3px] group">
+//       <div className=" hover:shadow-[3px_-3px_0_rgba(255,101,0)]  transition-all duration-300 relative z-10 w-[160px] h-[207.3px] lg:w-[281.44px] lg:h-[363.3px] group bg-[#FFFFFF] rounded-[9.2px] p-3 lg:p-5 ">
+//         <img
+//           className="w-[26px] h-[26px] lg:w-[46px] lg:h-[46px]"
+//           src={image}
+//           alt=""
+//         />
+//         <div className="space-y-4 lg:mt-5 mt-2 w-[133px] h-[135px] lg:w-[243px] lg:h-[237.79px]">
+//           <h3 className="font-Poppins h-[31px] lg:0 font-bold text-[10.58px] lg:text-[20.23px] lg:leading-[27.59px] text-left text-[#0E0E0E] lg:h-[55px]">
+//             {title}
+//           </h3>
+//           <p className="font-Poppins line-clamp-4 font-normal text-[7.37px] leading-[15.59px] lg:text-[12.88px] lg:leading-[27.59px] text-left text-[#444444]">
+//             {description}
+//           </p>
+//           <div className="flex gap-1 lg:gap-3 items-center">
+//             <div className="w-[21px] h-[21px] lg:w-[36px] lg:h-[36px] rounded-full bg-[#EBEBEB] flex items-center justify-center group-hover:bg-[#FF6500]">
+//               <FaPlus className="h-[7px] lg:h-[12px] text-[#FF6500] group-hover:text-[#ffff]" />
+//             </div>
+//             <span className="font-Poppins font-normal text-[7.837px] leading-[15.59px] lg:text-[12.88px] lg:leading-[27.59px] text-left text-[#232F3B]">
+//               Read More
+//             </span>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
 import { FaPlus } from "react-icons/fa";
 
 const Card = ({ title, description, image }) => {
   return (
-    <div className="cursor-pointer relative w-[160px] h-[207.3px] sm:w-[281.44px] sm:h-[363.3px] group">
-      <div className=" hover:shadow-[3px_-3px_0_rgba(255,101,0)]  transition-all duration-300 relative z-10 w-[160px] h-[207.3px] sm:w-[281.44px] sm:h-[363.3px] group bg-[#FFFFFF] rounded-[9.2px] p-3 sm:p-5 ">
+    <div className="cursor-pointer relative w-[160px] h-[207.3px] lg:w-[281.44px] lg:h-[363.3px] group">
+      <div className="hover:shadow-[3px_-3px_0_rgba(255,101,0)] transition-all duration-300 relative z-10 w-full h-full bg-[#FFFFFF] rounded-[9.2px] p-3 lg:p-5">
         <img
-          className="w-[26px] h-[26px] sm:w-[46px] sm:h-[46px]"
+          className="w-[26px] h-[26px] lg:w-[46px] lg:h-[46px]"
           src={image}
-          alt=""
+          alt={title}
         />
-        <div className="space-y-4 sm:mt-5 mt-2 w-[133px] h-[135px] sm:w-[243px] sm:h-[237.79px]">
-          <h3 className="font-Poppins h-[31px] sm:0 font-bold text-[10.58px] sm:text-[20.23px] sm:leading-[27.59px] text-left text-[#0E0E0E] sm:h-[55px]">
+        <div className="space-y-2 lg:space-y-4 mt-2 lg:mt-5">
+          <h3 className="font-Poppins font-bold text-[10.58px] lg:text-[20.23px] text-left text-[#0E0E0E] h-[31px] lg:h-[55px] leading-tight lg:leading-[27.59px]">
             {title}
           </h3>
-          <p className="font-Poppins line-clamp-4 font-normal text-[7.37px] leading-[15.59px] sm:text-[12.88px] sm:leading-[27.59px] text-left text-[#444444]">
+          <p className="font-Poppins font-normal text-[7.37px] lg:text-[12.88px] text-left text-[#444444] line-clamp-4 leading-[15.59px] lg:leading-[27.59px]">
             {description}
           </p>
-          <div className="flex gap-1 sm:gap-3 items-center">
-            <div className="w-[21px] h-[21px] sm:w-[36px] sm:h-[36px] rounded-full bg-[#EBEBEB] flex items-center justify-center group-hover:bg-[#FF6500]">
-              <FaPlus className="h-[7px] sm:h-[12px] text-[#FF6500] group-hover:text-[#ffff]" />
+          <div className="flex gap-1 lg:gap-3 items-center">
+            <div className="w-[21px] h-[21px] lg:w-[36px] lg:h-[36px] rounded-full bg-[#EBEBEB] flex items-center justify-center group-hover:bg-[#FF6500]">
+              <FaPlus className="h-[7px] lg:h-[12px] text-[#FF6500] group-hover:text-white" />
             </div>
-            <span className="font-Poppins font-normal text-[7.837px] leading-[15.59px] sm:text-[12.88px] sm:leading-[27.59px] text-left text-[#232F3B]">
+            <span className="font-Poppins font-normal text-[7.837px] lg:text-[12.88px] text-left text-[#232F3B] leading-[15.59px] lg:leading-[27.59px]">
               Read More
             </span>
           </div>
--- frontend/src/components/whatWeProvide/WhatWeProvide.jsx
+++ frontend/src/components/whatWeProvide/WhatWeProvide.jsx
@@ -1,4 +1,54 @@
 
+// import React from "react";
+// import bg from "../../assets/images/whatweprovidebg.svg";
+// import rightArrow from "../../assets/images/rightArrow.svg";
+// import WhatWeProvideCard from "./Card";
+// import { WhatWeProvideData } from "../../utils/fakedb/data";
+
+// const WhatWeProvide = () => {
+//   const backgroundImageStyle = {
+//     backgroundImage: `url(${bg})`,
+//     backgroundSize: "cover",
+//     backgroundPosition: "center",
+//   };
+//   return (
+//     <div className="relative mt-[23px] lg:mt-[40px]" style={backgroundImageStyle}>
+//       <div className="container w-[350px] sm:w-[645px] lg:w-[1184px] mx-auto px-4 sm:px-6 grid grid-cols-12 gap-4 pt-[63px] sm:pt-[37px] ">
+//         <h3 className="col-span-full font-Poppins font-semibold text-[12px] leading-[28.19px] sm:text-[16px] sm:leading-[28.19px] text-[#FF6500] tracking-[0.9396284818649292px] text-center">
+//           What We Provide
+//         </h3>
+//         <h3 className="col-span-full font-Poppins font-semibold text-[23px] leading-[34.19px] sm:text-[42px] sm:leading-[41.08px] text-[#232F3B] text-center">
+//           We provide truly prominent Software solutions
+//         </h3>
+//         <div className="col-span-full mt-6 sm:mt-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-1 gap-x-12  md:gap-x-4 xl:gap-x-20 mr-8">
+//           {WhatWeProvideData.map((card, index) => (
+//             <WhatWeProvideCard
+//               key={index}
+//               image={Object.values(card.img)[0]}
+//               title={card.title}
+//               description={card.description}
+//             />
+//           ))}
+//         </div>
+//         <div className="col-span-full flex items-center justify-center py-[5px] sm:pt-[30px] sm:pb-[48px]">
+//           <h3 className="font-Poppins font-normal text-[12px] sm:text-[16px] leading-[30px] text-center text-[#232F3B]">
+//             Learn more about{" "}
+//             <span className="font-Poppins font-semibold text-[12px] sm:text-[16px] leading-[30px] text-[#FF6500] cursor-pointer">
+//               More Services
+//             </span>
+//           </h3>
+//           <img
+//             className="object-contain cursor-pointer ml-2"
+//             src={rightArrow}
+//             alt=""
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default WhatWeProvide;
 import React from "react";
 import bg from "../../assets/images/whatweprovidebg.svg";
 import rightArrow from "../../assets/images/rightArrow.svg";
@@ -11,16 +61,17 @@ const WhatWeProvide = () => {
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
+
   return (
-    <div className="relative mt-[23px] lg:mt-[40px]" style={backgroundImageStyle}>
-      <div className="container w-[350px] sm:w-[645px] lg:w-[1184px] mx-auto px-4 sm:px-6 grid grid-cols-12 gap-4 pt-[63px] sm:pt-[37px] ">
-        <h3 className="col-span-full font-Poppins font-semibold text-[12px] leading-[28.19px] sm:text-[16px] sm:leading-[28.19px] text-[#FF6500] tracking-[0.9396284818649292px] text-center">
+    <div className="relative mt-6 lg:mt-10" style={backgroundImageStyle}>
+      <div className="container mx-auto px-4 sm:px-6 pt-10 w-full max-w-[1184px]">
+        <h3 className="font-Poppins font-semibold text-sm sm:text-base text-[#FF6500] tracking-wider text-center">
           What We Provide
         </h3>
-        <h3 className="col-span-full font-Poppins font-semibold text-[23px] leading-[34.19px] sm:text-[42px] sm:leading-[41.08px] text-[#232F3B] text-center">
+        <h3 className="font-Poppins font-semibold text-2xl sm:text-3xl lg:text-4xl text-[#232F3B] text-center mt-2">
           We provide truly prominent Software solutions
         </h3>
-        <div className="col-span-full mt-6 sm:mt-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-1 gap-x-12  md:gap-x-4 xl:gap-x-20 mr-8">
+        <div className="mt-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-4 lg:gap-8">
           {WhatWeProvideData.map((card, index) => (
             <WhatWeProvideCard
               key={index}
@@ -30,17 +81,17 @@ const WhatWeProvide = () => {
             />
           ))}
         </div>
-        <div className="col-span-full flex items-center justify-center py-[5px] sm:pt-[30px] sm:pb-[48px]">
-          <h3 className="font-Poppins font-normal text-[12px] sm:text-[16px] leading-[30px] text-center text-[#232F3B]">
+        <div className="flex items-center justify-center py-6 sm:py-10">
+          <h3 className="font-Poppins text-sm sm:text-base text-center text-[#232F3B]">
             Learn more about{" "}
-            <span className="font-Poppins font-semibold text-[12px] sm:text-[16px] leading-[30px] text-[#FF6500] cursor-pointer">
+            <span className="font-Poppins font-semibold text-[#FF6500] cursor-pointer">
               More Services
             </span>
           </h3>
           <img
             className="object-contain cursor-pointer ml-2"
             src={rightArrow}
-            alt=""
+            alt="Right Arrow"
           />
         </div>
       </div>
@@ -49,4 +100,3 @@ const WhatWeProvide = () => {
 };
 
 export default WhatWeProvide;
-
