d793a1dfcc7ff9908d12dfc37d7750add050e66d
Alihassan1536 <alihsnjanjua202@gmail.com>
2024-08-02 16:54:35 +0500

    slider changes done

--- frontend/src/components/OurClients.jsx
+++ frontend/src/components/OurClients.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import allcorlogo from "../assets/images/clients/allcorlogo.png";
-import attock from "../assets/images/clients/attock.jpeg";
-import caroyal from "../assets/images/clients/caroyal.png";
-import dindin from "../assets/images/clients/dindin.png";
-import elogo from "../assets/images/clients/elogo.jpeg";
-import fastCables from "../assets/images/clients/fastCables.jpeg";
-import officeEmpire from "../assets/images/clients/officeEmpire.png";
+import allcorlogo from "../assets/images/clients/allcorlogo_new.png";
+import attock from "../assets/images/clients/attock.png";
+import caroyal from "../assets/images/clients/caroyal_logo_new.png";
+import dindin from "../assets/images/clients/dindin-logo-new.png";
+import elogo from "../assets/images/clients/E.png";
+import fastCables from "../assets/images/clients/fastcables.png";
+import officeEmpire from "../assets/images/clients/Office empire.png";
 
 const OurClients = () => {
   const logos = [
@@ -40,14 +40,14 @@ const OurClients = () => {
       <h1 className="text-[#232F3B] font-Poppins font-semibold text-[20px] leading-[33.08px] md:text-[27px] sm:leading-[61.08px]  xl:text-[42px] xl:leading-[100.08px] text-center">
         We share our confidence and passion with.
       </h1>
-      <Slider {...settings} className="filter grayscale ">
+      <Slider {...settings} className="gap-4">
         {logos.map((logo, index) => (
-          <div key={index} className="flex items-center justify-center">
+          <div key={index} className="flex items-center justify-center gap-10">
             <img
               src={logo}
               alt={`Logo ${index}`}
               // className="w-[64px] h-[64px] md:w-[100px] md:h-[100px]  lg:w-[167.54px] lg:h-[167.54px]"
-              className="w-[64px] h-[64px] md:w-[100px] md:h-[100px]  lg:w-[167.54px] lg:h-[167.54px]"
+              className="w-[64px] h-[64px] md:w-[100px] md:h-[100px]  lg:w-[150px] lg:h-[158px]"
             />
           </div>
         ))}
